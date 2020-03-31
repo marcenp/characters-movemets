9a24e11e425db1633709e08fc590c11b4d023a98
mpuebla <mpuebla@focasoftware.com>
2020-03-31 17:32:52 -0300

    Fix Arreglo en codigo para mejor funcionamiento de rotacion de camara

--- Assets/Scripts/Player/ThirdPersonCameraControl.cs
+++ Assets/Scripts/Player/ThirdPersonCameraControl.cs
@@ -26,13 +26,11 @@ public class ThirdPersonCameraControl : MonoBehaviour
     mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
     mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
     mouseY = Mathf.Clamp(mouseY, -20, 60);
-
     transform.LookAt(targetCamera);
-
     if (Input.GetKey(KeyCode.LeftShift))
-    {
       targetCamera.rotation = Quaternion.Euler(mouseY, mouseX, 0);
-    }
+    else if (Input.GetKeyUp(KeyCode.LeftShift))
+      mouseX = 0;
     else
     {
       targetCamera.rotation = Quaternion.Euler(mouseY, mouseX, 0);
@@ -44,24 +42,12 @@ public class ThirdPersonCameraControl : MonoBehaviour
   {
     RaycastHit hit;
     if (!Physics.Raycast(transform.position, targetCamera.position - transform.position, out hit, 4.5f)) return;
+    if (hit.collider.gameObject.tag == "Player") return;
 
-    if (hit.collider.gameObject.tag != "Player")
-    {
-      obstruction = hit.transform;
-      obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
-      if (Vector3.Distance(obstruction.position, transform.position) >= 3f && Vector3.Distance(transform.position, targetCamera.position) >= 1.5f)
-        transform.Translate(Vector3.forward * zoomSpeed * Time.deltaTime);
-    }
-
+    obstruction = hit.transform;
+    obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+    if (Vector3.Distance(obstruction.position, transform.position) >= 3f && Vector3.Distance(transform.position, targetCamera.position) >= 1.5f)
+      transform.Translate(Vector3.forward * zoomSpeed * Time.deltaTime);
   }
 
-  IEnumerator LerpFromTo(Vector3 pos1, Vector3 pos2, float duration)
-  {
-    for (float t = 0f; t < duration; t += Time.deltaTime)
-    {
-      transform.position = Vector3.Lerp(pos1, pos2, t / duration);
-      yield return 0;
-    }
-    transform.position = pos2;
-  }
 }
