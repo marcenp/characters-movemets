e1af8184ecdcbdfa23035087ef5d9b88cbf78f73
mpuebla <mpuebla@focasoftware.com>
2020-03-31 16:43:24 -0300

    Add Primera version con trabajado ya realizado

---
+++ Assets/Scripts/Player/ThirdPersonCameraControl.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCameraControl : MonoBehaviour
+{
+  public float rotationSpeed = 1;
+  public Transform targetCamera, player;
+  Transform obstruction;
+  float mouseX, mouseY;
+  float zoomSpeed = 2f;
+
+  void Start()
+  {
+    obstruction = targetCamera;
+    Cursor.lockState = CursorLockMode.Locked;
+  }
+
+  private void LateUpdate()
+  {
+    CamControl();
+    ViewObstructed();
+  }
+
+  void CamControl()
+  {
+    mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+    mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+    mouseY = Mathf.Clamp(mouseY, -20, 60);
+
+    transform.LookAt(targetCamera);
+
+    if (Input.GetKey(KeyCode.LeftShift))
+    {
+      targetCamera.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+    }
+    else
+    {
+      targetCamera.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+      player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+  }
+
+  void ViewObstructed()
+  {
+    RaycastHit hit;
+    if (!Physics.Raycast(transform.position, targetCamera.position - transform.position, out hit, 4.5f)) return;
+
+    if (hit.collider.gameObject.tag != "Player")
+    {
+      obstruction = hit.transform;
+      obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+      if (Vector3.Distance(obstruction.position, transform.position) >= 3f && Vector3.Distance(transform.position, targetCamera.position) >= 1.5f)
+        transform.Translate(Vector3.forward * zoomSpeed * Time.deltaTime);
+    }
+
+  }
+
+  IEnumerator LerpFromTo(Vector3 pos1, Vector3 pos2, float duration)
+  {
+    for (float t = 0f; t < duration; t += Time.deltaTime)
+    {
+      transform.position = Vector3.Lerp(pos1, pos2, t / duration);
+      yield return 0;
+    }
+    transform.position = pos2;
+  }
+}
---
+++ Assets/Scripts/Player/ThirdPersonCharacterControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterControl : MonoBehaviour
+{
+  public float speed;
+  Animator animatorController;
+
+  void Awake()
+  {
+    animatorController = GetComponent<Animator>();
+  }
+
+  void Update()
+  {
+    Movement();
+  }
+
+  void Movement()
+  {
+    float horizontal = Input.GetAxis("Horizontal");
+    float vertical = Input.GetAxis("Vertical");
+    float auxSpeed = vertical < 0.5f ? speed * 0.1f : speed;
+    animatorController.SetFloat("speedVertical", vertical * speed);
+    animatorController.SetFloat("speedHorizontal", horizontal * speed);
+    Vector3 playerMovement = new Vector3(horizontal * 0.75f, 0f, vertical) * auxSpeed * Time.deltaTime;
+    transform.Translate(playerMovement, Space.Self);
+  }
+}
